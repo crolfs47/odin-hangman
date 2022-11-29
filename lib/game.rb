0b1f2f95ef5a320ea8464aec61c8e03e17f65caa
Cathy Rolfs <crolfs47@gmail.com>
2022-11-29 11:17:06 -0600

    Add methods to evaluate guess, update guessed word, and check if game over

--- lib/game.rb
+++ lib/game.rb
@@ -4,12 +4,15 @@ class Game
     @guess_count = 10
     @game_over = false
     @word = select_random_word
+    @guessed_word = ['_'] * @word.length
     @incorrect_guesses = []
+    @correct_guesses = []
     play_game
   end
 
   def play_game
     p @word
+    p @guessed_word.join
     take_turn until @game_over
   end
 
@@ -25,6 +28,7 @@ class Game
   def take_turn
     guess = guess_letter
     evaluate_guess(guess)
+    check_game_over
   end
 
   def guess_letter
@@ -38,5 +42,28 @@ class Game
     end
   end
 
-  def evaluate_guess(letter); end
+  def evaluate_guess(letter)
+    if @word.include?(letter)
+      puts 'Good guess!'
+      update_guessed_word(letter)
+    else
+      puts "Sorry, #{letter} is not in the word!"
+      @guess_count -= 1
+      puts "Incorrect guesses remaining: #{@guess_count}"
+    end
+  end
+
+  def update_guessed_word(guess)
+    @word.split('').each_with_index do |_letter, index|
+      @guessed_word[index] = guess if guess == @word[index]
+    end
+    p @guessed_word
+  end
+
+  def check_game_over
+    if @guess_count.zero?
+      @game_over = true
+      puts "You ran out of guesses! The word you were trying to guess was: #{@word}"
+    end
+  end
 end
